c5451b19d0715ae38fa2b10e9f109a1ee8d86c39
Pushpendra V <pushpendra.hpx2001@gmail.com>
2022-01-07 23:37:32 +0530

    Till Now

--- components/MusicPlayer/MusicPlayer.js
+++ components/MusicPlayer/MusicPlayer.js
@@ -1,13 +1,41 @@
 import React from "react"
+import styles from "./../../styles/MusicPlayer.module.scss";
 
+
+import PlayCircleFilledWhiteOutlinedIcon from '@mui/icons-material/PlayCircleFilledWhiteOutlined';
+import PauseCircleOutlineOutlinedIcon from '@mui/icons-material/PauseCircleOutlineOutlined';
+import SkipNextOutlinedIcon from '@mui/icons-material/SkipNextOutlined';
+import SkipPreviousOutlinedIcon from '@mui/icons-material/SkipPreviousOutlined';
+// const ComfortFit = require("")
+// const audioTune = new Audio('./../../public/lib/comfort_fit.mp3');
 class MusicPlayer extends React.Component{
     constructor(props){
         super(props)
-        
+        this.state = {
+            status:true
+        }
     }
     render(){
-        return (<div>
-            Music Player
+        return (<div className={styles["player-container"]}>
+            <div className={styles["player-main-container"]}>
+                <div className={styles["player-title"]}>
+
+                    <marquee>Circular Box Shadow CSS</marquee>
+                </div>                
+                <audio src={"https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Creative_Commons/Comfort_Fit/Forget_And_Remember/Comfort_Fit_-_03_-_Sorry.mp3?download=1&name=Comfort%20Fit%20-%20Sorry.mp3"} controls />
+{/* 
+                <div className={styles["player-actions"]}>
+                    <div className={styles["player-actions-previous"]}> <SkipPreviousOutlinedIcon /> </div>
+                    <div className={styles["player-actions-pause-play"]} onClick={()=>{
+                        this.setState(prev=>{
+                            return {status:!prev.status}
+                        })
+                    }}> 
+                        {this.state.status === true ? <PlayCircleFilledWhiteOutlinedIcon /> : <PauseCircleOutlineOutlinedIcon />}
+                    </div>
+                    <div className={styles["player-actions-previous"]}> <SkipNextOutlinedIcon /> </div>
+                </div> */}
+            </div>
         </div>);
     }
 }
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,6 +1,14 @@
 import Link from "next/link";
 import MusicPlayer from "../MusicPlayer/MusicPlayer";
-import styles from "./../../styles/Navbar.module.scss"
+import styles from "./../../styles/Navbar.module.scss";
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import SelfImprovementIcon from '@mui/icons-material/SelfImprovement';
+import TimerOutlinedIcon from '@mui/icons-material/TimerOutlined';
+import MusicNoteOutlinedIcon from '@mui/icons-material/MusicNoteOutlined';
+import NoteAltOutlinedIcon from '@mui/icons-material/NoteAltOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+
 const Navbar = ()=>{
     return <div className={styles["navbar-container"]}>
         <div className={styles["navbar-brand"]}>
@@ -12,18 +20,18 @@ const Navbar = ()=>{
         <div className={styles["navbar-actions"]}>
             <ul>
                 <li>
-                    <Link href="/gym">gym</Link>
+                    <Link href="/gym"><FitnessCenterIcon /></Link>
                 </li>
                 <li>
-                    <Link href="/yoga">yoga</Link>
+                    <Link href="/yoga"><SelfImprovementIcon /></Link>
                 </li>
                 <li>
-                    <Link href="/timer">timer</Link>
+                    <Link href="/timer"><TimerOutlinedIcon /></Link>
                 </li>
-                <li><Link href="/music">music</Link></li>
-                <li><Link href="/notepad">notepad</Link></li>
-                <li><Link href="/profile">profile</Link></li>
-                <li><Link href="/settings">settings</Link></li>
+                <li><Link href="/music"><MusicNoteOutlinedIcon /></Link></li>
+                <li><Link href="/notepad"><NoteAltOutlinedIcon /></Link></li>
+                <li><Link href="/profile"><AccountCircleOutlinedIcon /></Link></li>
+                <li><Link href="/settings"><SettingsOutlinedIcon /></Link></li>
             </ul>
         </div>
     </div>
