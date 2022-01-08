2f6b2101340aae30c0c90c2ccaf0e210bab69161
Harsh Suthar <bernaullis@gmail.com>
2022-01-08 23:30:02 +0530

    game css fixed + yoga pose added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
 import Layout from '../components/layout'
 import Navbar from '../components/Navbar/Navbar'
+import Router from 'next/router'
+import { useEffect, useState } from 'react'
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }) {
     console.log("_app rendered") 
+    const [hideNav, sethideNav] = useState(true);
+    useEffect(() => {
+      if(typeof window !='undefined' && (Router.pathname === '/fitnessGame')){
+        sethideNav(false)
+        console.log("hideNav", hideNav)
+      }
+    }, [hideNav]);
   return <Layout>
-      <Navbar />
+      {hideNav && <Navbar/>}
       <Component {...pageProps} />
   </Layout>
 }
